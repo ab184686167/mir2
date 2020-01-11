0ece5d79ac21a3ac01c46d4ebe7e9b8796c0fb54
zhzhwcn <zhzhwmail@gmail.com>
2020-01-11 17:41:32 +0800

    add .net core run entry project

---
+++ Server.NetCore/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Server.MirEnvir;
+namespace Server.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomainOnProcessExit;
+            Console.CancelKeyPress += ConsoleOnCancelKeyPress;
+            Envir.Main.Start();
+            var messageQueue = MessageQueue.Instance;
+            while (true)
+            {
+                while (messageQueue.MessageLog.TryDequeue(out var msg))
+                {
+                    Console.Write($"[{DateTime.Now}][Message]:{msg}");
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        private static void ConsoleOnCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.WriteLine("^C Pressed... Stop Server...");
+            Envir.Main.Stop();
+            Environment.Exit(0);
+        }
+
+        private static void CurrentDomainOnProcessExit(object sender, EventArgs e)
+        {
+            
+            Console.WriteLine("Exiting...");
+            Envir.Main.Stop();
+            //Environment.Exit(-1);
+        }
+    }
+}
